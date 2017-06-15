87e40a902d5ed09daa67821858fabfb1cb51ae9e
Ahmet Alp Balkan <ahmetb@google.com>
2017-06-15 16:15:39 -0700

    WIP: simple web server
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

---
+++ sample-web-server/README.md
@@ -0,0 +1,9 @@
+# Sample Web Server application
+
+This directory contains source file for a web server application that responds
+to all requests on port 8080 with a page containing details of the request,
+response and environment variables of the server. If running on GCE/GKE, it
+lists the details about the instance, zone, and project ID.
+
+It is used in various tutorials and examples to demonstrate a HTTP web server
+container that returns HTML content and has no additional capabilities.
---
+++ sample-web-server/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"cloud.google.com/go/compute/metadata"
+)
+
+const (
+	httpAddr  = ":8080"
+	httpsAddr = ":443"
+)
+
+var (
+	tpl      = template.Must(template.ParseFiles("template.html"))
+	hostname string
+)
+
+func init() {
+	h, err := os.Hostname()
+	if err != nil {
+		panic(fmt.Errorf("cannot get hostname: %+v", err))
+	}
+	hostname = h
+}
+
+func main() {
+	http.HandleFunc("/", htmlHandler)
+
+	var addr string
+	tlsDir := os.Getenv("TLS_DIR")
+	if tlsDir != "" {
+		addr = httpsAddr
+	} else {
+		addr = httpAddr
+	}
+
+	log.Printf("running on gce: %v", metadata.OnGCE())
+	log.Println("starting web server on " + addr)
+	if tlsDir == "" {
+		log.Fatal(http.ListenAndServe(addr, http.DefaultServeMux))
+	} else {
+		log.Fatal(http.ListenAndServeTLS(addr, filepath.Join(tlsDir, "tls.crt"), filepath.Join(tlsDir, "tls.key"), nil))
+	}
+}
+
+func htmlHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("handing request on " + r.URL.String())
+	envs := os.Environ()
+	env := make(map[string]string)
+	for _, e := range envs {
+		ss := strings.SplitN(e, "=", 2)
+		env[ss[0]] = ss[1]
+	}
+
+	data := map[string]interface{}{
+		"env":      env,
+		"req":      r,
+		"hostname": hostname,
+		"onGCE":    metadata.OnGCE(),
+	}
+
+	// Fetch instance metadata if on GCE/GKE
+	if metadata.OnGCE() {
+		val := func(s string, _ error) string { return s }
+		data["gce"] = map[string]string{
+			"hostname":     val(metadata.Hostname()),
+			"projectID":    val(metadata.ProjectID()),
+			"instanceName": val(metadata.InstanceName()),
+			"instanceID":   val(metadata.InstanceID()),
+			"zone":         val(metadata.Zone()),
+		}
+	}
+
+	tpl.Execute(w, data)
+}
