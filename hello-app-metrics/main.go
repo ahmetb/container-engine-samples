c023ee7f1e71930978dec40112c3e761c40984c5
Ahmet Alp Balkan <ahmetb@google.com>
2018-04-25 15:56:20 -0700

    Sample prom app
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

---
+++ hello-app-metrics/main.go
@@ -0,0 +1,74 @@
+/**
+ * Copyright 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// [START all]
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	httpRequests = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "http_requests_total",
+		Help: "total requests to HTTP endpoints",
+	}, nil)
+	httpRequestPaths = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "http_request_paths",
+		Help: "total requests to HTTP endpoints by path",
+	}, []string{"path"})
+)
+
+func init() {
+	prometheus.MustRegister(httpRequests)
+	prometheus.MustRegister(httpRequestPaths)
+}
+
+func main() {
+	// use PORT environment variable, or default to 8080
+	port := "8080"
+	if fromEnv := os.Getenv("PORT"); fromEnv != "" {
+		port = fromEnv
+	}
+
+	// register hello function to handle all requests
+	server := http.NewServeMux()
+	server.HandleFunc("/", promhttp.InstrumentHandlerCounter(httpRequests, http.HandlerFunc(hello)))
+	server.HandleFunc("/metrics", promhttp.Handler().ServeHTTP)
+
+	// start the web server on port and accept requests
+	log.Printf("Server listening on port %s", port)
+	err := http.ListenAndServe(":"+port, server)
+	log.Fatal(err)
+}
+
+// hello responds to the request with a plain-text "Hello, world" message.
+func hello(w http.ResponseWriter, r *http.Request) {
+	httpRequestPaths.With(prometheus.Labels{"path": r.URL.Path}).Inc()
+	log.Printf("Serving request: %s", r.URL.Path)
+	host, _ := os.Hostname()
+	fmt.Fprintf(w, "Hello, world!\n")
+	fmt.Fprintf(w, "Version: 1.0.0\n")
+	fmt.Fprintf(w, "Hostname: %s\n", host)
+}
+
+// [END all]
